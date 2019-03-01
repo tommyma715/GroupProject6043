f69a9c9d9bb3a6be59783ce94698d60e8104fa1f
unrealandychan <unrealandychan@gmail.com>
2019-03-01 13:04:18 +0800

    New

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class travelIdeasController extends Controller
+class IdeaController extends Controller
 {
     //
 }
--- app/Idea.php
+++ app/Idea.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class comments extends Model
+class Idea extends Model
 {
     //
 }
